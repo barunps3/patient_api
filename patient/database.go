c1e77461bc2c9581495819922fa0a344e48dd785
barun <barunps3@gmail.com>
2023-07-22 12:55:17 +0200

    added db server api version

--- patient/database.go
+++ patient/database.go
@@ -9,7 +9,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-const mongo_uri = "mongodb+srv://master:Master_1@cluster0.sqvis.mongodb.net/?w=majority"
+const mongo_uri = "mongodb+srv://master:Master_1@cluster0.sqvis.mongodb.net/?retryWrites=true&w=majority"
 
 const dbName string = "test"
 const patientDetails string = "patient_details"
@@ -18,7 +18,8 @@ const patientCTScans string = "patient_ctscans"
 const patientAppointments string = "patient_appointments"
 
 func getDbClient() *mongo.Client {
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mongo_uri))
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mongo_uri).SetServerAPIOptions(serverAPI))
 	if err != nil {
 		panic(err)
 	}
