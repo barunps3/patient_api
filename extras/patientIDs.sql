a5d03c21c8dc26d665fa7e7c942c5156b5ceccb3
barun <barunps3@gmail.com>
2023-12-10 21:45:59 +0100

    updated patientidsql

--- extras/patientIDs.sql
+++ extras/patientIDs.sql
@@ -4,8 +4,8 @@ create table patientIDs (
   idValue VARCHAR(50) NOT NULL,
   PRIMARY KEY(patientUUID, idType)
 );
-insert into patientIDs (patientUUID, idType, idValue) values ('1c340c57-f8e9-4471-b3dc-4cfc1533a90c','passport', 'Q6rnz6m45MAb');
-insert into patientIDs (patientUUID, idType, idValue) values ('1c340c57-f8e9-4471-b3dc-4cfc1533a90c', 'aadharcard', 'a0d1I5b50ye9');
-insert into patientIDs (patientUUID, idType, idValue) values ('91792716-1724-4a14-8d36-b1599be750b0', 'aadharcard', '1G2AOYsv1eqv');
-insert into patientIDs (patientUUID, idType, idValue) values ('a27d0dda-67fc-449f-85cb-83b137d305fd', 'aadharcard', 'NN6qZh43PxbP');
-insert into patientIDs (patientUUID, idType, idValue) values ('74d27551-87d8-4cae-8d21-c9a31b7d2571', 'passport', 'G2QwwFndzH1A');
+insert into patientIDs (patientUUID, idType, idValue) values ('1c340c57-f8e9-4471-b3dc-4cfc1533a90c','Passport', 'Q6rnz6m45MAb');
+insert into patientIDs (patientUUID, idType, idValue) values ('1c340c57-f8e9-4471-b3dc-4cfc1533a90c', 'AadharCard', 'a0d1I5b50ye9');
+insert into patientIDs (patientUUID, idType, idValue) values ('91792716-1724-4a14-8d36-b1599be750b0', 'AadharCard', '1G2AOYsv1eqv');
+insert into patientIDs (patientUUID, idType, idValue) values ('a27d0dda-67fc-449f-85cb-83b137d305fd', 'AadharCard', 'NN6qZh43PxbP');
+insert into patientIDs (patientUUID, idType, idValue) values ('74d27551-87d8-4cae-8d21-c9a31b7d2571', 'Passport', 'G2QwwFndzH1A');
